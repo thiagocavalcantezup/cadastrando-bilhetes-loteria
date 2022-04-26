cf1775c20ee13a0d0a94bf8a891e7d5b179a2de6
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-26 02:46:14 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/cadastrandobilhetesloteria/CadastrandoBilhetesLoteriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrandobilhetesloteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrandoBilhetesLoteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastrandoBilhetesLoteriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/handora/cadastrandobilhetesloteria/CadastrandoBilhetesLoteriaApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.handora.cadastrandobilhetesloteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrandoBilhetesLoteriaApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
